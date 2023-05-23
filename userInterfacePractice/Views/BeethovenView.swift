6adfe05317d41bf5a56f501a8f27bc1460f423df
David Li <dtli@lcs.on.ca>
2023-05-23 11:48:52 -0400

    finish the raw planing of editor's choice sections are the Beethoven's image sections. Need to add detail (view modifier and different font and color)

--- userInterfacePractice/Views/BeethovenView.swift
+++ userInterfacePractice/Views/BeethovenView.swift
@@ -10,7 +10,48 @@ import SwiftUI
 struct BeethovenView: View {
     var body: some View {
         ScrollView{
-            
+            VStack{
+                //Image of Beethoven
+                ZStack{
+                    Image("")
+                    VStack{
+                        Spacer()
+                        HStack{
+                            VStack{
+                                HStack{
+                                    Text("Composer")
+                                    Text(".")
+                                    Text("1770 - 1827")
+                                }
+                                VStack{
+                                    Text("Ludwig Van")
+                                    Text("Beethoven")
+                                }
+                            }
+                            Spacer()
+                        }
+                    }
+                }
+                //Editor's choice
+                HStack{
+                    Image("")
+                    VStack{
+                        Text("Editor's choice")
+                        Text("Beethoven: Symphonies Nos. 4 & 5")
+                        Button(action: {
+                            
+                        }, label: {
+                            HStack{
+                                Image(systemName: "play.fill")
+                                Text("Play")
+                            }
+                        })
+                        .buttonStyle(.borderedProminent)
+                    }
+                }
+                
+                // Popular work
+            }
         }
     }
 }
