5ac882b95acbf6fbacb2ebcb58a5227674673fa1
David Li <dtli@lcs.on.ca>
2023-05-23 11:39:03 -0400

    First commit, remove contentView, Create Beethoven View, decide on make UI of Apple music classical

---
+++ userInterfacePractice/Views/BeethovenView.swift
@@ -0,0 +1,22 @@
+//
+//  BeethovenView.swift
+//  userInterfacePractice
+//
+//  Created by David Li on 2023-05-23.
+//
+
+import SwiftUI
+
+struct BeethovenView: View {
+    var body: some View {
+        ScrollView{
+            
+        }
+    }
+}
+
+struct BeethovenView_Previews: PreviewProvider {
+    static var previews: some View {
+        BeethovenView()
+    }
+}
--- userInterfacePractice/userInterfacePracticeApp.swift
+++ userInterfacePractice/userInterfacePracticeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct userInterfacePracticeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BeethovenView()
         }
     }
 }
