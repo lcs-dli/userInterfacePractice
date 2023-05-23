309ca947eaeb43a94860020f11c8a9829c722b5c
David Li <dtli@lcs.on.ca>
2023-05-23 11:35:45 -0400

    Initial Commit

---
+++ userInterfacePractice/userInterfacePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  userInterfacePracticeApp.swift
+//  userInterfacePractice
+//
+//  Created by David Li on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct userInterfacePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
